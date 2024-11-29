eb7b7a1fb74953c01258d4014c31b61c744f017e
Bassikousky <flopmar186@iesfuengirola1.es>
2024-11-29 18:26:20 +0100

    añado pantalla de game over

--- Attacards/Assets/FuncionesBotones.cs
+++ Attacards/Assets/FuncionesBotones.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement; // Para cambiar de escena
 public class FuncionesBotones : MonoBehaviour
 {
     public string escenaInicio;
-    
+    public string escenaMenu;
+
 
     public void empezarJuego()
     {
@@ -22,5 +23,19 @@ public class FuncionesBotones : MonoBehaviour
     public void salirJuego()
     {
         Debug.Log("Salir del juego");
+        Application.Quit();
+
     }
+
+    public void volverMenu()
+    {
+        if (!string.IsNullOrEmpty(escenaMenu))
+        {
+            SceneManager.LoadScene(escenaMenu);
+        }
+        else
+        {
+            Debug.LogError("No se ha especificado el nombre de la escena a cargar.");
+        }
+    } 
 }
